275467d8b85a9db567e57113139f85d3b60555b6
charanamanawathilake <charanamanawathilake@gmail.com>
2025-05-27 11:47:53 +0530

    Setup docker

--- UserManagmentService/src/main/java/com/usermanagement/usermanagmentservice/UserManagmentServiceApplication.java
+++ UserManagmentService/src/main/java/com/usermanagement/usermanagmentservice/UserManagmentServiceApplication.java
@@ -2,12 +2,11 @@ package com.usermanagement.usermanagmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UserManagmentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserManagmentServiceApplication.class, args);
     }
-
 }
