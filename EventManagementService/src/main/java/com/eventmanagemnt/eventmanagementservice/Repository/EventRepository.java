fb95772284579533d859a7d2fdc3f5b8782054a3
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-06-07 22:03:09 +0530

    fix issues in EventManagementServiceApplication

--- EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/Repository/EventRepository.java
+++ EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/Repository/EventRepository.java
@@ -3,9 +3,11 @@ package com.eventmanagemnt.eventmanagementservice.Repository;
 import com.eventmanagemnt.eventmanagementservice.Model.Event;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EventRepository extends MongoRepository<Event, String> {
     List<Event> findByHostId(String hostId);
     List<Event> findByCategory(String category);
