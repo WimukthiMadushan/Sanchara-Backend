d301e3b12201216d781a0caef1b359785ecd4fe3
Wimukthi_madushan <129815767+WimukthiMadushan@users.noreply.github.com>
2025-06-07 21:28:36 +0530

    Delete WishListManagmentService directory

--- WishListManagmentService/src/main/java/com/wishlistmanagement/wishlistmanagementservice/Dto/WishListRequest.java
+++
@@ -1,8 +0,0 @@
-package com.wishlistmanagement.wishlistmanagementservice.Dto;
-
-public record WishListRequest(
-        String id,
-        String eventId,
-        String userId
-) {
-}
--- WishListManagmentService/src/main/java/com/wishlistmanagement/wishlistmanagementservice/Dto/WishListResponse.java
+++
@@ -1,8 +0,0 @@
-package com.wishlistmanagement.wishlistmanagementservice.Dto;
-
-public record WishListResponse(
-        String id,
-        String eventId,
-        String userId
-) {
-}
