8db16279a0d3cc5425cd466ebe8510cbde8c3adc
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-06-07 20:44:14 +0530

    complete EmailService and EventManagementService

--- EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
+++ EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
@@ -1,24 +1,12 @@
-<<<<<<<< HEAD:EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
-package com.eventmanagemnt.eventmanagementservice;
-========
 package com.emailservice.emailservice;
->>>>>>>> 7dbdb487521907b4fc059018523208b557c1d1e5:EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<<< HEAD:EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
-public class EventManagementServiceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EventManagementServiceApplication.class, args);
-========
 public class EmailServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmailServiceApplication.class, args);
->>>>>>>> 7dbdb487521907b4fc059018523208b557c1d1e5:EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
     }
 
 }
--- EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
+++ EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
@@ -1,24 +1,13 @@
-<<<<<<<< HEAD:EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
 package com.eventmanagemnt.eventmanagementservice;
-========
-package com.emailservice.emailservice;
->>>>>>>> 7dbdb487521907b4fc059018523208b557c1d1e5:EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-<<<<<<<< HEAD:EventManagementService/src/main/java/com/eventmanagemnt/eventmanagementservice/EventManagementServiceApplication.java
+@EnableMongoRepositories(basePackages  = "com.eventmanagemnt.eventmanagementservice.Repository")
 public class EventManagementServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EventManagementServiceApplication.class, args);
-========
-public class EmailServiceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EmailServiceApplication.class, args);
->>>>>>>> 7dbdb487521907b4fc059018523208b557c1d1e5:EmailService/src/main/java/com/emailservice/emailservice/EmailServiceApplication.java
     }
-
 }
